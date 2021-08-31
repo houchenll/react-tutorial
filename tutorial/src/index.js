c37b1814780870daf299359c9307bb465e69d8d2
houchenll <houchenll@163.com>
2021-08-31 10:08:06 +0800

    Taking Turns

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -18,6 +18,7 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
@@ -31,12 +32,15 @@ class Board extends React.Component {
   handleClick(i) {
     // slice()，创建squares数组的一个副本
     const squares = this.state.squares.slice();
-    squares[i] = 'X'
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
